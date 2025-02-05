3c9da433a17496d11b01fcf96830edd470ec3171
69pmb <pmbroca@gmail.com>
2025-02-05 18:10:19 +0100

    wip

--- src/main/java/pmb/recipes/domain/service/RecipeService.java
+++ src/main/java/pmb/recipes/domain/service/RecipeService.java
@@ -1,18 +1,17 @@
 package pmb.recipes.domain.service;
 
+import java.util.List;
 import java.util.Optional;
-
+import java.util.stream.Stream;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
-
 import pmb.recipes.adapter.rest.dto.RecipeDto;
 import pmb.recipes.adapter.rest.dto.SearchRecipeDto;
 import pmb.recipes.adapter.rest.mapper.RecipeMapper;
-import pmb.recipes.domain.model.Recipe;
 import pmb.recipes.domain.repository.RecipeRepository;
 import pmb.recipes.domain.specification.RecipeSpecification;
 
@@ -43,32 +42,36 @@ public class RecipeService {
   }
 
   public Page<RecipeDto> search(SearchRecipeDto search, Pageable pageable) {
-    Specification<Recipe> spec = Specification.where(null);
-    if (search.cookingTimeMax() != null && search.cookingTimeMin() != null) {
-      spec = spec
-          .and(RecipeSpecification.betweenTime(search.cookingTimeMin(), search.cookingTimeMax(), "cookingTime"));
+    /*Specification<Recipe> spec = Specification.where(null);
+    if(search.cookingTimeMax() != null && search.cookingTimeMin() != null) {
+        spec = spec
+            .and(RecipeSpecification.betweenTime(search.cookingTimeMin(), search.cookingTimeMax(), "cookingTime"));
     }
-    if (search.preparationTimeMax() != null && search.preparationTimeMin() != null) {
-      spec = spec
-          .and(RecipeSpecification.betweenTime(search.preparationTimeMin(), search.preparationTimeMax(),
-              "preparationTime"));
-    }
-    if (StringUtils.isNotBlank(search.title())) {
-      spec = spec
-          .and(RecipeSpecification.containsTitle(search.title()));
-    }
-    if (search.season() != null) {
-      spec = spec
-          .and(RecipeSpecification.isSeason(search.season()));
-    }
-    if (CollectionUtils.isNotEmpty(search.difficulties())) {
-      spec = spec
-          .and(RecipeSpecification.in(search.difficulties(), "difficulty"));
-    }
-    if (CollectionUtils.isNotEmpty(search.nutriscores())) {
-      spec = spec
-          .and(RecipeSpecification.in(search.nutriscores(), "nutriscore"));
-    }
-    return recipeMapper.toDtoPage(recipeRepository.findAll(spec, pageable));
+    if(search.preparationTimeMax() != null && search.preparationTimeMin() != null) {
+        spec = spec
+            .and(RecipeSpecification.betweenTime(search.preparationTimeMin(), search.preparationTimeMax(),
+                                                 "preparationTime"));
+    }*/
+    return recipeMapper.toDtoPage(
+        recipeRepository.findAll(
+            Specification.allOf(
+                Stream.of(
+                        Optional.ofNullable(search.title())
+                            .filter(StringUtils::isNotBlank)
+                            .map(RecipeSpecification::containsTitle),
+                        Optional.ofNullable(search.title())
+                            .filter(StringUtils::isNotBlank)
+                            .map(RecipeSpecification::containsTitle),
+                        Optional.ofNullable(search.season()).map(RecipeSpecification::isSeason),
+                        Optional.ofNullable(search.difficulties())
+                            .filter(CollectionUtils::isNotEmpty)
+                            .map(d -> RecipeSpecification.in(List.of(d), "difficulty")),
+                        Optional.ofNullable(search.nutriscores())
+                            .filter(CollectionUtils::isNotEmpty)
+                            .map(d -> RecipeSpecification.in(List.of(d), "nutriscore")))
+                    .filter(Optional::isPresent)
+                    .map(Optional::get)
+                    .toList()),
+            pageable));
   }
 }
